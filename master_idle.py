29fc0bdbf8384aabc95d2c9cff2faf8d37cd0a57
Parit_03 <45036413+Parit99@users.noreply.github.com>
2021-03-01 23:07:19 +0530

    Add files via upload

---
+++ master_idle.py
@@ -0,0 +1,35 @@
+import sys,io,os,math
+from math import ceil,log,gcd,sqrt
+from itertools import permutations
+import operator
+mod=1000000007
+mod1=998244353   
+def intinp():
+    return int(sys.stdin.readline())
+def strinp():
+    return sys.stdin.readline()
+def arrinp():
+    return list(map(int,sys.stdin.readline().strip().split()))
+def mulinp():
+    return map(int,sys.stdin.readline().strip().split())
+def flush():
+    return stdout.flush()
+def power_two(x):
+    return (1<<x)
+def lcm(a,b):
+	return a*b//gcd(a,b)
+
+
+
+def solve():
+    pass
+    
+
+def main():
+	#tc=intinp()
+	tc=1
+	while(tc):
+		solve()
+		tc-=1	
+main()
+
